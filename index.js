315bc048176d7db5195d4c69968637de209bb2c0
Osgood <osgoodackom@gmail.com>
2022-12-11 01:52:52 +0000

    worked on the innerHTML functionality

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const addListButton = document.getElementById("add-list")
 const textSection = document.getElementById("text-section")
 const taskSection = document.getElementById("task-section")
 const taskForm = document.getElementById("task-form")
+const myArr = []
+
 
 taskForm.addEventListener("submit", addItem)
 
 function addItem (e){
     e.preventDefault();
-    const taskValue = textSection.value;
-    console.log(taskValue)
-    taskValue.innerHTML = taskSection
+    let taskValue = textSection.value;
+    myArr.push(taskValue)
+    localStorage.setItem("myArr", JSON.stringify("myArr"))
+    moveToHtmlInput()
+}
+
+function moveToHtmlInput(){
+    let listItem = ""
+    for (let i = 0; i < myArr.length; i++){
+        listItem += myArr[i] + " ";
+    }
+    taskSection.innerHTML = listItem;
 }
