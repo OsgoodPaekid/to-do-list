ac4817449a7a236232c52c840fdceb2eb87cb6ac
Osgood <osgoodackom@gmail.com>
2022-12-12 23:39:39 +0000

    worked on the alert display

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const textSection = document.getElementById("text-section")
 const taskSection = document.getElementById("task-section")
 const taskForm = document.getElementById("task-form")
 const myArr = []
+const alertSection = document.getElementById("alert-section")
+let editItem = false;
 
 
 taskForm.addEventListener("submit", addItem)
@@ -14,6 +16,12 @@ function addItem (e){
     localStorage.setItem("taskValue", textSection.value)
     localStorage.getItem("taskValue")
     moveToHtmlInput()
+    // if (taskValue === ""){
+    //     setTimeout(() => {
+    //         alertSection.innerHTML = "no task entered"
+    //     }, 3000)
+    // }
+    removeDiv()
 }
 
 function moveToHtmlInput(){
@@ -22,4 +30,20 @@ function moveToHtmlInput(){
         listItem += `<ul>${myArr[i]}</ul>`;
     }
     taskSection.innerHTML = listItem;
+}
+
+const storedValue = localStorage.getItem("taskValue")
+        if(storedValue !== false){
+            myArr.push(storedValue)
+        }
+        moveToHtmlInput()
+
+
+function removeDiv(){
+    let div = document.createElement("div")
+    alertSection.innerHTML = "No Task Entered"
+    document.h2.appendChild(div)
+    setTimeout(function(){
+        div.parentNode.removeChild(div)
+    }, 2000)
 }
